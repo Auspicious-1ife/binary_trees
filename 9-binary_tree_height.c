b1087ce4d129960d3e04bc3d4e5af861afee22ac
Auspicious-1ife <melissagushi50@gmail.com>
2024-06-04 16:08:11 +0000

    a function that measures the height of a binary tree, corrected parenthesis on the return statement

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,8 +11,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 size_t left_height, right_height;
 if (tree == NULL)
 return (0);
-left_height = binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
-return ((left_height > right_height) ? right_height + 1 : left_height + 1);
+left_height = binary_tree_height(tree->left);
+return ((left_height != 0 ? left_height + 1 : right_height + 1));
 }
 
